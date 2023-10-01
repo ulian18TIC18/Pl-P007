d1b3a7019337bc2d9c1054251517d63c626f1629
ulian18TIC18 <ulian18@gmail.com>
2023-09-30 22:07:03 -0300

    Resolução do Exercício 2 corrigido

---
+++ exer2_pl_p007.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1, original, inverso = 0, digito;
+
+    cout << "Digite um numumero inteiro: ";
+    cin >> num1;
+
+    original = num1;  
+
+    while (num1 > 0) {
+        digito = num1 % 10;
+        inverso = inverso * 10 + digito;
+     num1 /= 10;
+
+    }
+    string resultado = (original == inverso) ? 
+    " eh palindromo" : "Nao eh palindromo";
+
+    cout << "O nummero digitado: " << resultado << endl;
+
+    return 0;
+}
