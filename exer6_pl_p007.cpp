f5d38857ae7cdb758b88a6c6cc420973467330fb
ulian18TIC18 <ulian18@gmail.com>
2023-10-02 14:51:50 -0300

    Resolução do exercício 6 Primeira parte

---
+++ exer6_pl_p007.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main(){
+
+    int matricula;
+    double nota1, nota2, nota3, media;
+    char continuar;
+
+    do{
+        cout << "Digite o numero de matricula do aluno: ";
+        cin >> matricula;
+
+        cout << "Digite a nota 1 do aluno: ";
+        cin >> nota1;
+
+        cout << "Digite a nota 2 do aluno: ";
+        cin >> nota2;
+
+        cout << "Digite a nota 3 do aluno: ";
+        cin >> nota3;
+
+        media = (nota1 + nota2 + nota3) / 3.0;
+
+        cout << fixed << setprecision(1); 
+        cout << "===========================================" << endl;
+        cout << "MATRICULA\tNOTA1\tNOTA2\tNOTA3\tMEDIA" << endl;
+        cout << "===========================================" << endl;
+        cout << matricula << "\t\t" << nota1 << "\t" << nota2 << "\t" 
+        << nota3 << "\t" << media << endl;
+
+        cout << "Deseja cadastrar outro aluno? (S/N): ";
+        cin >> continuar;
+
+    } 
+    while(continuar == 'S' || continuar == 's');
+
+return 0;
+}
